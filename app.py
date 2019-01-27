869c33a3d1f22f8768cdefd2e04ab246305dd7fa
Alan Wang <yumaowang.tw@gmail.com>
2019-01-27 20:36:10 +0800

    talking

--- app.py
+++ app.py
@@ -36,9 +36,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+
+    if msg in ["組長好", "你好" ,"您好"]:
+        re = "我是新上任的核儀組組長，大家好!!"
+    elif msg in ["張永瑞","張東西","髒"]:
+        re = "我正在出國爽，等我回國再說"
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=re))
 
 
 if __name__ == "__main__":
